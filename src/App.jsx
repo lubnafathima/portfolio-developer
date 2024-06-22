bf12aed142f528d66bd9cc1464260af75bb645af
lubna-sensegrass <lubna@sensegrass.com>
2024-06-22 17:06:14 +0530

    feat: Added about section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import About from './components/About/About';
+
+function App() {
+
+  return (
+    <div className='app'>
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import Logo from "../../assets/icons/logo.jpeg";
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="main">
+        <img src={Logo} alt="logo" className="logo" />
+        <h1 className="title">
+          I&#39;m Lubna — a frontend{" "}
+          <span className="developer">developer</span>, passionate about
+          crafting intuitive user experiences.
+        </h1>
+      </div>
+      <div className="divider"></div>
+      <div className="about">
+        <p className="about_content">
+          My journey began with a fascination for technology during my early
+          years, which evolved into a career driven by a commitment to
+          excellence in coding and user experience design.
+        </p>
+        <br />
+        <p className="about_content">
+          I have honed my skills in translating design concepts into functional
+          web applications.
+        </p>
+        <br />
+        <p className="about_content">
+          My expertise spans across modern frontend technologies such as HTML5,
+          CSS3, JavaScript, and various frameworks like React and Next JS,
+          enabling me to create dynamic and responsive interfaces that elevate
+          user interaction.
+        </p>
+      </div>
+      <div className="about_divider"></div>
+      </>
+  );
+};
+
+export default About;
